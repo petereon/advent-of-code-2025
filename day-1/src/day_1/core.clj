3ba7fef52e9b655f1a80cc47f1b7b96455ff414d
Peter Výboch <pvyboch1@gmail.com>
2025-12-04 23:30:42 +0100

    feat: solving the second part of day-1

--- day-1/src/day_1/core.clj
+++ day-1/src/day_1/core.clj
@@ -1,25 +1,33 @@
 (ns day-1.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
+(defn parse-rotation [rotation]
+  (when-not (str/blank? rotation)
+    {:direction (first rotation)
+     :steps (Integer/parseInt (subs rotation 1))}))
 
-(defn rotate-dial [current_position rotation]
-  (let [[direction & steps] rotation]
-    (if (= direction \R)
-      (mod (+ current_position (Integer/parseInt (apply str steps))) 100)
-      (mod (- current_position (Integer/parseInt (apply str steps))) 100))))
+(defn rotate-dial [position {:keys [direction steps]}]
+  (mod ((if (= direction \R) + -) position steps) 100))
 
+(defn count-zero-passes [position {:keys [direction steps]}]
+  (quot (if (or (= direction \R) (= position 0))
+          (+ position steps)
+          (+ (- 100 position) steps))
+        100))
 
-(defn process-lines [lines current_position current_count]
-  (if (or (empty? lines) (= (first lines) "") )
-    current_count
-    (let [[head & rest] lines
-          new_current_position (rotate-dial current_position head)]
-      (recur rest new_current_position  (if (== new_current_position 0) (+ current_count 1) current_count)))))
+(defn process-rotations [rotations initial-position]
+  (->> rotations
+       (remove str/blank?)
+       (map parse-rotation)
+       (reduce (fn [[pos count] rotation]
+                 (let [new-pos (rotate-dial pos rotation)
+                       passes (count-zero-passes pos rotation)]
+                   [new-pos (+ count passes)]))
+               [initial-position 0])
+       second))
 
 (defn -main []
   (with-open [rdr (io/reader "data/input.txt")]
-    (println (process-lines (line-seq rdr) 50 0))))
-
-
-
+    (println (process-rotations (line-seq rdr) 50))))
