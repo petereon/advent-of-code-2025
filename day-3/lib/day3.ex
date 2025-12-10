aa36ca7d4ea1763ea158beb7a25d3fed7e4345eb
Peter Výboch <pvyboch1@gmail.com>
2025-12-10 20:12:59 +0100

    chore: more cleanup

--- day-3/lib/day3.ex
+++ day-3/lib/day3.ex
@@ -1,14 +1,13 @@
 defmodule Day3 do
   require File
-  # require Stream
+
   def find_largest_joltage(bank, num_batteries) do
     list = bank |> String.graphemes |> Enum.map(&String.to_integer/1)
     batteries = find_largest_in_sublist(list, num_batteries, [])
     String.to_integer(Enum.join(batteries))
   end
 
-
-def find_largest_in_sublist(sublist, num_batteries, found) do
+  def find_largest_in_sublist(sublist, num_batteries, found) do
     if sublist == [] or Enum.count(found) == num_batteries do
       found
     else
@@ -23,8 +22,6 @@ def find_largest_in_sublist(sublist, num_batteries, found) do
       find_largest_in_sublist(Enum.drop(sublist, index_of_max + 1), num_batteries, found ++ [max_val_sublist])
     end
   end
-
-
 
   def main do
     stream = File.stream!("data/input.txt")
